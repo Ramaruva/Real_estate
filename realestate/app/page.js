c4e9788452f033f1c00b4b6ea6a9fb0e3e6b671a
Ramaruva <ramaruva54@gmail.com>
2023-10-30 22:31:43 -0500

    added banner component

--- realestate/app/page.js
+++ realestate/app/page.js
@@ -2,16 +2,15 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import Link from 'next/link'
-const Banner=()=>{
+const Banner=({purpose,title1,title2,linkName,buttonText,desc1,desc2,imgeUrl})=>{
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imgeUrl} width={500} height={300} alt='banner'/>
     <Box p="5"> 
       <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
       <Text fontSize="3x1" fontWeight="bold">{title1}<br/>{title2}</Text>
-      <Text color="gray.700" fontSize="lg" paddingTop="3" paddingBottom="3" fontWeight="medium">{desc1}</Text>
+      <Text color="gray.700" fontSize="lg" paddingTop="3" paddingBottom="3" fontWeight="medium">{desc1}<br />{desc2}</Text>
       <Button fontSize="x1" bg="blue.300" color="white">
-         <Link></Link>
-         <Image/>
+         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
